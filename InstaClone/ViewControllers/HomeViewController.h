a2eb7f74913b13bcd07c2692b4630809bd270789
abbyli437 <31529490+abbyli437@users.noreply.github.com>
2021-07-06 13:47:30 -0700

    implemented logout
    implemented logout function

---
+++ InstaClone/ViewControllers/HomeViewController.h
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.h
+//  InstaClone
+//
+//  Created by Abby Li on 7/6/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HomeViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ InstaClone/ViewControllers/HomeViewController.m
@@ -0,0 +1,45 @@
+//
+//  HomeViewController.m
+//  InstaClone
+//
+//  Created by Abby Li on 7/6/21.
+//
+
+#import "HomeViewController.h"
+#import "Parse/Parse.h"
+#import "LoginViewController.h"
+#import "AppDelegate.h"
+#import "SceneDelegate.h"
+
+@interface HomeViewController ()
+
+@end
+
+@implementation HomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)logoutTap:(id)sender {
+    [PFUser logOutInBackgroundWithBlock:^(NSError * _Nullable error) {
+        // PFUser.current() will now be nil
+        SceneDelegate *sceneDelegate = (SceneDelegate *)self.view.window.windowScene.delegate;
+        UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+        LoginViewController *loginViewController = [storyboard instantiateViewControllerWithIdentifier:@"LoginViewController"];
+        sceneDelegate.window.rootViewController = loginViewController;
+    }];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
